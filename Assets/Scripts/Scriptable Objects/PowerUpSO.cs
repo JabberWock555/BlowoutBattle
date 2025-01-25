6d8bc7dd4be1b857e3f3ca0abf231bba7c324396
Adi Patil <4dipatil@gmail.com>
2025-01-25 22:22:18 +0530

    ui manager moved to coop

---
+++ Assets/Scripts/Scriptable Objects/PowerUpSO.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Powerups", menuName = "Scriptable Objects/Powerups")]
+public class PowerUpSO : ScriptableObject
+{
+    public PowerUpType powerUpType;
+    public Image image;
+}
